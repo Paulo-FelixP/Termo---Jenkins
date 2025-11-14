de84f940f277a50af8a3d9df90d0d09e9f3bf3f0
Paulo Felix <Paulo.felixper@gmail.com>
2025-11-14 02:05:29 -0300

    atualizacao testes
    usando testes reais

--- jogo_termo/termo/tests.py
+++ jogo_termo/termo/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.http import HttpResponse
 
-# Create your tests here.
+class TesteBasicoTermo(TestCase):
+    def test_pagina_inicial_carrega(self):
+        """
+        Testa se a página inicial do projeto responde com status 200.
+        Caso sua view principal tenha outro nome, basta ajustar a URL.
+        """
+
+        url = reverse('index')  # <- ajuste aqui caso sua URL tenha outro nome
+        response = self.client.get(url)
+
+        # 1) A página deve existir
+        self.assertEqual(response.status_code, 200)
+
+        # 2) A página deve conter algum texto esperado
+        self.assertIn(b"Termo", response.content)
