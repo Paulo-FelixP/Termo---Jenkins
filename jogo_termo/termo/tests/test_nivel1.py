fb950bdd1460bb23ee143960ca96dfae729561d9
Paulo Felix <Paulo.felixper@gmail.com>
2025-11-15 01:12:28 -0300

    Consertando Test
    Agora que deu erro, estou alterando para ver se sera corrigido o erro.

--- jogo_termo/termo/tests/test_nivel1.py
+++ jogo_termo/termo/tests/test_nivel1.py
@@ -14,4 +14,4 @@ class TesteNivel1(TestCase):
         response = self.client.get(url)
 
         # teste propositalmente errado
-        self.assertTemplateUsed(response, 'nao_existe_teste.html')
+        self.assertTemplateUsed(response, 'termo/nivel1.html')
